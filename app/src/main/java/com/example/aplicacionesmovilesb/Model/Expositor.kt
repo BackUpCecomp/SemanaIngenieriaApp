2ac7146adf4c682c12d80373b7ce19ff88d8f671
MeloDev <melodiaz2111@gmail.com>
2022-08-16 11:24:08 -0500

    feat(firebase): model expositor

---
+++ app/src/main/java/com/example/aplicacionesmovilesb/Model/Expositor.kt
@@ -0,0 +1,16 @@
+package com.example.aplicacionesmovilesb.Model
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import org.json.JSONArray
+
+class Expositor(){
+    var name = ""
+    var job = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var img = ""
+    var category = 0
+    
+}
